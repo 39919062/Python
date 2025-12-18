cd037ea188bc1537e125d847407b4d96d9508cf6
Mauri <106289038+39919062@users.noreply.github.com>
2025-12-18 17:21:37 -0300

    Ejercicio de transmisión de datos
    Una red transmite datos a  100  megabits por segundo. Crea una función utilizando Python que permita calcular la cantidad de datos transmitidos en una cierta cantidad de tiempo (en segundos). Luego utiliza la función y calcula cuántos datos se transmiten en:
    
    45  segundos
    1,5  minutos
    1  hora

---
+++ Ejercicio de transmite datos.py
@@ -0,0 +1,6 @@
+def transmite_megabites(m):
+  return 100 * m
+
+print(f"En 45 segundos la red transmite:         {transmite_megabites(45)} megabits por segundo")
+print(f"En un minuto y medio la red transmite:   {transmite_megabites(90)} megabits por segundo")
+print(f"En una hora la red transmite:            {transmite_megabites(3600)} megabits por segundo")
